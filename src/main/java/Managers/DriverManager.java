8903a2e04d43eee7a9ad2455a19468bb3cc14401
yobhdl <110323935+yobhdl@users.noreply.github.com>
2023-11-02 15:01:13 +0530

    Update DriverManager.java

--- src/main/java/Managers/DriverManager.java
+++ src/main/java/Managers/DriverManager.java
@@ -14,6 +14,14 @@ public class DriverManager {
         switch (browser) {
             case "chrome":
                 WebDriverManager.chromedriver().setup();
+                ChromeOptions options = new ChromeOptions();
+                options.addArguments("start-maximized"); // open Browser in maximized mode
+                options.addArguments("disable-infobars"); // disabling infobars
+                options.addArguments("--disable-extensions"); // disabling extensions
+                options.addArguments("--disable-gpu"); // applicable to windows os only
+                options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
+                options.addArguments("--no-sandbox"); // Bypass OS security model
+                WebDriver driver = new ChromeDriver(options);
                 driver = new ChromeDriver();
                 break;
 
