59fbb444bfa9a9e55ad994f44817cca0fe148e3e
yobhdl <110323935+yobhdl@users.noreply.github.com>
2023-11-02 15:11:56 +0530

    Update DriverManager.java 2

--- src/main/java/Managers/DriverManager.java
+++ src/main/java/Managers/DriverManager.java
@@ -22,8 +22,7 @@ public class DriverManager {
                 options.addArguments("--disable-gpu"); // applicable to windows os only
                 options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
                 options.addArguments("--no-sandbox"); // Bypass OS security model
-                WebDriver driver = new ChromeDriver(options);
-                driver = new ChromeDriver();
+                driver = new ChromeDriver(options);
                 break;
 
             case "firefox":
